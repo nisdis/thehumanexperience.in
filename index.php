64ea74fac19efd3170faacf73d2c8fccd656fe42
nisdis <niszim@gmail.com>
2021-08-15 13:45:23 +0530

    Change title of website

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<!-- <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"> -->
 
-	<title>Hope With Corona</title>
+	<title>THX: Release your true potential</title>
 	<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans|Cabin&display=swap" rel="stylesheet">
 	<!-- <link href="https://fonts.googleapis.com/css?family=Cabin|Ubuntu&display=swap" rel="stylesheet"> -->
 
